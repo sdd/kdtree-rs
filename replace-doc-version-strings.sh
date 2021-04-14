19b73c193f67cb7a332447619a339a48bff16be6
Scott Donnelly <scott@donnel.ly>
2021-04-14 17:21:48 +0100

    ci: generalize replace-doc-version-strings.sh. update version in Cargo.lock

--- replace-doc-version-strings.sh
+++ replace-doc-version-strings.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
-sed "s%^//! kiddo = \".*\"%//! kiddo = \"$1\"%" < src/lib.rs > new-lib.rs
-sed "s%docs\.rs/kiddo/.*\"%docs.rs/kiddo/$1\"%" < new-lib.rs > src/lib.rs
-sed "s%^kiddo = \".*\"%kiddo = \"$1\"%" < README.md > new-README.md
+sed "s%^//! $1 = \".*\"%//! $1 = \"$2\"%" < src/lib.rs > new-lib.rs
+sed "s%docs\.rs/$1/.*\"%docs.rs/$1/$2\"%" < new-lib.rs > src/lib.rs
+sed "s%^$1 = \".*\"%$1 = \"$2\"%" < README.md > new-README.md
 mv new-README.md README.md
 rm new-lib.rs
